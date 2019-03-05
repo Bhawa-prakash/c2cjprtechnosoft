b56703f5e6afdd0a5e6be8f2910b97466e584442
Bhawa prakash <35968702+Bhawa-prakash@users.noreply.github.com>
2019-03-05 20:35:44 +0530

    Initial commit

---
+++ app/src/main/java/com/e/c2cjprtechnosoft/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.e.c2cjprtechnosoft;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText _editMobile, _editPassword;
+    private TextView newUser;
+    private Button _loginbtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.loginactivity);
+        _editMobile = findViewById(R.id.mobile);
+        _editPassword = findViewById(R.id.password);
+        TextView newUser = (TextView) findViewById(R.id.newuser);
+        Button _loginbtn = ( Button)findViewById(R.id.loginbutton);
+
+        newUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(i);
+            }
+        });
+        _loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(LoginActivity.this, NavigationActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+    }
+}
